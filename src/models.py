813c741bf0ff57899d102b8d334b139b4ae8089b
Oscar Urrutia <oscarurrutia940@gmail.com>
2022-09-05 19:06:02 +0000

    ok

--- src/models.py
+++ src/models.py
@@ -1,6 +1,6 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String,Enum
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
@@ -8,28 +8,53 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class User(Base):
+    __tablename__ = 'user'
+   
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    username = Column(String(250), nullable=False , unique=True)
+    firstname = Column(String(250), nullable=False , unique=True)
+    lastname = Column(String(250), nullable=False , unique=True)
+    email = Column(String(250), nullable=False , unique=True)
+    
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Follower(Base):
+    __tablename__ = 'follower'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    user_from_id = Column(Integer, ForeignKey('user.id'))
+    user_to_id =  Column(Integer, ForeignKey('user.id'))
 
-    def to_dict(self):
-        return {}
 
-## Draw from SQLAlchemy base
+class Media(Base):
+    __tablename__ = 'media'
+   
+    id = Column(Integer, primary_key=True)
+    type = Column(Enum , nullable=False , unique=True,)
+    url =Column(String(250), nullable=False , unique=True)
+    post_id =Column(Integer, ForeignKey('post.id'),nullable=False , unique=True)
+
+
+class Post(Base):
+    __tablename__ = 'post'
+   
+    id = Column(Integer, primary_key=True)
+    user_id =Column(Integer, ForeignKey('user.id'))
+
+
+class Comment(Base):
+    __tablename__ = 'Comment'
+   
+    id = Column(Integer, primary_key=True)
+    coment_text = Column(String(250), nullable=False , unique=True)
+    author_id =Column(Integer, ForeignKey('user.id'),nullable=False , unique=True)
+    post_id =Column(Integer, ForeignKey('post.id'),nullable=False , unique=True)
+   
+   
+   
+   
+  
+
+
 try:
     result = render_er(Base, 'diagram.png')
     print("Success! Check the diagram.png file")
